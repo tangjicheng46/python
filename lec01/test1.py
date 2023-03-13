5686c0f29eede4f94c88e50857099ee7e1b91798
tangjicheng <tangjicheng@tencent.com>
2023-03-13 21:14:03 +0800

    lec01

---
+++ lec01/test1.py
@@ -0,0 +1,12 @@
+# x = '''\
+# There is one subtle ; 
+# more information and workarounds.\
+# '''
+# print(x)
+
+a = "xxxffffffffffffffff"    \
+"yddddddddddddyy"
+b = ("xxxffffffffffffffff"      "yddddddddddddyy")
+
+print(a)
+print(b)
